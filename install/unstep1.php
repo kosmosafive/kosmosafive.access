78d3f0f0de2ef84c3e4a40c6fe9e44c55b102395
Siarhei Buinevich <a5-kosmos@yandex.ru>
2025-11-27 12:36:47 +0300

    fix unstep

--- install/unstep1.php
+++ install/unstep1.php
@@ -13,7 +13,7 @@ Loc::loadMessages(__FILE__);
 <form action="<?= $APPLICATION->GetCurPage() ?>">
     <?= bitrix_sessid_post() ?>
     <input type="hidden" name="lang" value="<?= LANGUAGE_ID ?>">
-    <input type="hidden" name="id" value="kosmos.access">
+    <input type="hidden" name="id" value="kosmosafive.access">
     <input type="hidden" name="uninstall" value="Y">
     <input type="hidden" name="step" value="2">
     <?php
